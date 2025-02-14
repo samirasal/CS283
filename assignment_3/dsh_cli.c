5f708640364685e691b4dc336c9bfec1903b0303
samirasal <samirasalifu5522@gmail.com>
2025-02-13 22:35:41 -0500

    still running tests

--- assignment_3/dsh_cli.c
+++ assignment_3/dsh_cli.c
@@ -44,12 +44,61 @@
  *
  *  See the provided test cases for output expectations.
  */
+
 int main()
 {
-    char *cmd_buff;
-    int rc = 0;
-    command_list_t clist;
+    char cmd_buff[SH_CMD_MAX];  
+    command_list_t clist;       
 
-    printf(M_NOT_IMPL);
-    exit(EXIT_NOT_IMPL);
+    while (1)
+    {
+        printf("%s", SH_PROMPT);  
+
+        if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL)
+        {
+            printf("\n");
+            break;
+        }
+
+        cmd_buff[strcspn(cmd_buff, "\n")] = '\0';  
+
+        if (strcmp(cmd_buff, EXIT_CMD) == 0)
+        {
+            exit(0);  
+        }
+        else if (strcmp(cmd_buff, "dragon") == 0) // Extra credit feature
+        {
+            printf("[DRAGON for extra credit would print here]\n");
+            continue;
+        }
+
+        int rc = build_cmd_list(cmd_buff, &clist);
+
+        
+        if (rc == WARN_NO_CMDS)
+        {
+            printf("%s", CMD_WARN_NO_CMD);
+        }
+        else if (rc == ERR_TOO_MANY_COMMANDS)
+        {
+            printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+        }
+        else
+        {
+            printf(CMD_OK_HEADER, clist.num);
+            for (int i = 0; i < clist.num; i++)
+            {
+                if (strlen(clist.commands[i].args) > 0)
+                {
+                    printf("<%d> %s [%s]\n", i + 1, clist.commands[i].exe, clist.commands[i].args);
+                }
+                else
+                {
+                    printf("<%d> %s\n", i + 1, clist.commands[i].exe);
+                }
+            }
+        }
+    }
+
+    return 0;
 }
--- assignment_3/dshlib.c
+++ assignment_3/dshlib.c
@@ -32,8 +32,69 @@
  *  Standard Library Functions You Might Want To Consider Using
  *      memset(), strcmp(), strcpy(), strtok(), strlen(), strchr()
  */
-int build_cmd_list(char *cmd_line, command_list_t *clist)
+
+// Helper function to trim leading and trailing spaces
+void trim_spaces(char *str)
 {
-    printf(M_NOT_IMPL);
-    return EXIT_NOT_IMPL;
+    if (str == NULL || *str == '\0')
+        return;
+
+    // Trim leading spaces
+    while (*str == SPACE_CHAR)
+        str++;
+
+    // Trim trailing spaces
+    char *end = str + strlen(str) - 1;
+    while (end > str && *end == SPACE_CHAR)
+        *end-- = '\0';
+}
+
+
+
+
+ int build_cmd_list(char *cmd_line, command_list_t *clist)
+{
+    if (cmd_line == NULL || strlen(cmd_line) == 0)
+    {
+        return WARN_NO_CMDS; 
+    }
+
+    memset(clist, 0, sizeof(command_list_t)); 
+
+    char *token = strtok(cmd_line, PIPE_STRING); 
+    int cmd_count = 0;
+
+    while (token != NULL)
+    {
+        if (cmd_count >= CMD_MAX)
+        {
+            return ERR_TOO_MANY_COMMANDS; 
+        }
+        trim_spaces(token);
+        
+        // while (*token == SPACE_CHAR) token++;
+        // char *end = token + strlen(token) - 1;
+        // while (end > token && *end == SPACE_CHAR) *end-- = '\0';
+
+      
+        char *cmd_name = strtok(token, " ");
+        if (cmd_name == NULL)
+            return WARN_NO_CMDS; 
+
+        strncpy(clist->commands[cmd_count].exe, cmd_name, EXE_MAX - 1);
+        clist->commands[cmd_count].exe[EXE_MAX - 1] = '\0';
+        
+        char *args = strtok(NULL, ""); 
+        if (args)
+        {
+            strncpy(clist->commands[cmd_count].args, args, ARG_MAX - 1);
+            clist->commands[cmd_count].args[ARG_MAX - 1] = '\0';
+        }
+        
+        cmd_count++;
+        token = strtok(NULL, PIPE_STRING); 
+    }
+
+    clist->num = cmd_count;
+    return OK; 
 }
